68e931d1efc4a1e7eeddcc6bde61d127e6d84cf0
Aya <ayaabdelsalam91@github.com>
2019-09-17 02:33:20 -0400

    cleaning code

--- Scripts/createSimulationData.py
+++ Scripts/createSimulationData.py
@@ -1,7 +1,29 @@
 import numpy as np
 import Helper
-from heatmap import *
-Loc_Graph = '../Graphs/'
+import sys
+import argparse
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def strTovalue(value):
+	if ('[' not in value):
+		return int(value)
+	else:
+		value= value.replace("[", "")
+		value = value.replace("]", "")
+		value= value.replace(" ", "")
+		value = value.split(',')
+
+		return list(map(int, value))
+
 
 def createSample(Target,sampleSize,TargetSize,TargetYStart,TargetYEnd,TargetXStart,TargetXEnd,multipleBox):
 	sample=np.random.normal(0,1,sampleSize)
@@ -29,143 +51,60 @@ def createDataset(NumberOFsamples,sampleSize,TargetSize,TargetYStart,TargetYEnd,
 	return DataSet , Targets
 
 
-
-
-#TrainingDataset
-#number of features 100
-#number of timeSteps 10
-
-TS = 100
-NumFeatures=100
-ImpTS=30
-startTS=0
-endTS= 30
-startFeatures= 10
-endFeatures = 90
-ImpFeatures=80
-DataName="TopBox"
-DataName_="Ealier Box"
-multipleBox=False
-
-
-# TS = 100
-# NumFeatures=100
-# ImpTS=30
-# startTS=70
-# endTS= 100
-# startFeatures= 10
-# endFeatures = 90
-# ImpFeatures=80
-# DataName="BottomBox"
-# DataName_="Latter Box"
-# multipleBox=False
-
-
-# TS = 100
-# NumFeatures=100
-# ImpTS=60
-# startTS=20
-# endTS= 80
-# startFeatures= 20 
-# endFeatures = 80
-# ImpFeatures=60
-# DataName="MiddleBox"
-# multipleBox=False
-
-# TS = 70
-# NumFeatures=100
-# ImpTS=20
-# startTS=0
-# endTS= 20
-# startFeatures= 60 
-# endFeatures = 100
-# ImpFeatures=40
-# DataName="UpperRight"
-# multipleBox=False
-
-# TS = 70
-# NumFeatures=100
-# ImpTS=20
-# startTS=0
-# endTS= 20
-# startFeatures= 0 
-# endFeatures = 40
-# ImpFeatures=40
-# DataName="UpperLeft"
-# multipleBox=False
-
-
-# TS = 100
-# NumFeatures=100
-# ImpTS=[40,40,40]
-# startTS=[0,0,0]
-# endTS= [40,40,40]
-# startFeatures=[0,35,70]
-# endFeatures= [ 30,65, 100]
-# ImpFeatures=[30,30,30]
-# DataName="ThreeUpperBoxes"
-# multipleBox=True
+def main(args):
+	TS = args.NumTimeSteps
+	NumFeatures=args.NumFeatures
+	ImpTS=strTovalue(args.ImpTimeSteps)
+	startTS=strTovalue(args.StartImpTimeSteps)
+	endTS= strTovalue(args.EndImpTimeSteps)
+	startFeatures= strTovalue(args.StartImpFeatures)
+	endFeatures = strTovalue(args.EndImpFeatures)
+	ImpFeatures=strTovalue(args.ImpFeatures)
+	DataName=args.DataName
+	multipleBox=args.multipleBox
 
 
 
-# TS = 100
-# NumFeatures=100
-# ImpTS=[40,40,40]
-# startTS=[25,25,25]
-# endTS= [65,65,65]
-# startFeatures=[0,35,70]
-# endFeatures= [ 30,65, 100]
-# ImpFeatures=[30,30,30]
-# DataName="ThreeMiddleBoxes"
-# multipleBox=True
+	print("Creating Training Dataset")
+
+	TrainingDataset  , TrainingLabel= createDataset(args.NumTrainingSamples,[TS,NumFeatures],[ImpTS,ImpFeatures],startTS,endTS,startFeatures,endFeatures,multipleBox=multipleBox)
+	newTrainingLabel =  np.zeros((args.NumTrainingSamples,2))
+	newTrainingLabel[:,0]=TrainingLabel
+	newTrainingLabel[:,1]=TS
 
 
 
-print(DataName)
-TrainingDataset  , TrainingLabel= createDataset(400,[TS,NumFeatures],[ImpTS,ImpFeatures],startTS,endTS,startFeatures,endFeatures,multipleBox=multipleBox)
-newTrainingLabel =  np.zeros((400,2))
-newTrainingLabel[:,0]=TrainingLabel
-newTrainingLabel[:,1]=TS
-# ### Plotting####
-# negIndices = [i for i, x in enumerate(TrainingLabel) if x == -1]
-# posIndices = [i for i, x in enumerate(TrainingLabel) if x == 1]
-# max = np.amax(TrainingDataset)  
-# min = np.amin(TrainingDataset)  
-# posExamplesRNN =TrainingDataset[posIndices,:,:] 
-# meanPosExamples = np.mean(posExamplesRNN, axis=0)
+	print("Creating Testing Dataset")
+	TestingDataset ,TestingLabel= createDataset(args.NumTestingSamples,[TS,NumFeatures],[ImpTS,ImpFeatures],startTS,endTS,startFeatures,endFeatures,multipleBox=multipleBox)
+	newTestingLabel =  np.zeros((args.NumTestingSamples,2))
+	newTestingLabel[:,0]=TestingLabel
+	newTestingLabel[:,1]=TS
 
-# plotHeatMapExampleWise(meanPosExamples,"","_posMean"+DataName,max=max,min=min, flip=True,x_axis="Time",y_axis="Feature Value")
-# negExamplesRNN =TrainingDataset[negIndices,:,:] 
-# meanNegExamples = np.mean(negExamplesRNN, axis=0)
-# plotHeatMapExampleWise(meanNegExamples,"","_negMean"+DataName,max=max, min=min  , flip=True,x_axis="Time",y_axis="Feature Value")
-# ########
+	print("Saving Datasets...")
+	TrainingDataset=TrainingDataset.reshape((TrainingDataset.shape[0],TrainingDataset.shape[1]*TrainingDataset.shape[2]))
+	TestingDataset=TestingDataset.reshape((TestingDataset.shape[0],TestingDataset.shape[1]*TestingDataset.shape[2]))
+	data_dir="../Data/"
+	Helper.save_intoCSV(TrainingDataset,data_dir+"SimulatedTraining"+DataName+"_F"+str(NumFeatures)+"_TS_"+str(TS)+"_IMPSTART_"+str(startTS)+".csv")
+	Helper.save_intoCSV(newTrainingLabel,data_dir+"SimulatedTrainingLabels"+DataName+"_F"+str(NumFeatures)+"_TS_"+str(TS)+"_IMPSTART_"+str(startTS)+".csv")
+	Helper.save_intoCSV(TestingDataset,data_dir+"SimulatedTestingData"+DataName+"_F"+str(NumFeatures)+"_TS_"+str(TS)+"_IMPSTART_"+str(startTS)+".csv")
+	Helper.save_intoCSV(newTestingLabel,data_dir+"SimulatedTestingLabels"+DataName+"_F"+str(NumFeatures)+"_TS_"+str(TS)+"_IMPSTART_"+str(startTS)+".csv")
 
+def parse_arguments(argv):
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--DataName', type=str, default="TopBox")
+	parser.add_argument('--NumTrainingSamples',type=int,default=1000)
+	parser.add_argument('--NumTestingSamples',type=int,default=300)
+	parser.add_argument('--NumTimeSteps',type=int,default=100)
+	parser.add_argument('--NumFeatures',type=int,default=100)
+	parser.add_argument('--ImpTimeSteps',type=str,default=30)
+	parser.add_argument('--ImpFeatures',type=str,default=80)
+	parser.add_argument('--StartImpTimeSteps',type=str,default=0)
+	parser.add_argument('--EndImpTimeSteps',type=str,default=30)
+	parser.add_argument('--StartImpFeatures',type=str,default=10)
+	parser.add_argument('--EndImpFeatures',type=str,default=90)
+	parser.add_argument('--multipleBox',type=str2bool ,default=False)
+	return  parser.parse_args()
 
+if __name__ == '__main__':
+	main(parse_arguments(sys.argv[1:]))
 
-
-TestingDataset  ,  TestingLabel= createDataset(1000,[TS,NumFeatures],[ImpTS,ImpFeatures],startTS,endTS,startFeatures,endFeatures,multipleBox=multipleBox)
-newTestingLabel =  np.zeros((1000,2))
-newTestingLabel[:,0]=TestingLabel
-newTestingLabel[:,1]=TS
-
-### Plotting####
-# negIndices = [i for i, x in enumerate(TestingLabel) if x == -1]
-# posIndices = [i for i, x in enumerate(TestingLabel) if x == 1]
-# max = np.amax(TestingDataset)  
-# min = np.amin(TestingDataset)  
-# posExamplesRNN =TestingDataset[posIndices,:,:] 
-# meanPosExamples = np.mean(posExamplesRNN, axis=0)
-# plotHeatMapExampleWise(meanPosExamples,"Mean Postive " + DataName,"posMean"+DataName,max=max,min=min)
-# negExamplesRNN =TestingDataset[negIndices,:,:] 
-# meanNegExamples = np.mean(negExamplesRNN, axis=0)
-# plotHeatMapExampleWise(meanNegExamples,"Mean Negative " + DataName,"negMean"+DataName,max=max, min=min , flip=True,x_axis="Time",y_axis="Features")
-########
-
-
-TrainingDataset=TrainingDataset.reshape((TrainingDataset.shape[0],TrainingDataset.shape[1]*TrainingDataset.shape[2]))
-TestingDataset=TestingDataset.reshape((TestingDataset.shape[0],TestingDataset.shape[1]*TestingDataset.shape[2]))
-data_dir="../Data/"
-Helper.save_intoCSV(TrainingDataset,data_dir+"SimulatedTraining"+DataName+"_F"+str(NumFeatures)+"_TS_"+str(TS)+"_IMPSTART_"+str(startTS)+".csv")
-Helper.save_intoCSV(newTrainingLabel,data_dir+"SimulatedTrainingLabels"+DataName+"_F"+str(NumFeatures)+"_TS_"+str(TS)+"_IMPSTART_"+str(startTS)+".csv")
-Helper.save_intoCSV(TestingDataset,data_dir+"SimulatedTestingData"+DataName+"_F"+str(NumFeatures)+"_TS_"+str(TS)+"_IMPSTART_"+str(startTS)+".csv")
-Helper.save_intoCSV(newTestingLabel,data_dir+"SimulatedTestingLabels"+DataName+"_F"+str(NumFeatures)+"_TS_"+str(TS)+"_IMPSTART_"+str(startTS)+".csv")
